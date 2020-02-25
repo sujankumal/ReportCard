212339289aae1f889ecd2ef1167225026624c8e3
sujan kumal <mailtosujankumal@gmail.com>
2020-02-25 21:41:26 +0545

    models name changed

--- StudentExamReport/admin.py
+++ StudentExamReport/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Grade
+from .models import Student
+from .models import Subject
+from .models import StudentSubject
+from .models import Exam
+from .models import Result
+from .models import ResultComment
 
 # Register your models here.
+
+admin.site.register([
+        Grade, 
+        Student,  
+        Subject,
+        StudentSubject,
+        Exam, 
+        Result, 
+        ResultComment])
--- StudentExamReport/models.py
+++ StudentExamReport/models.py
@@ -5,32 +5,32 @@ class UserProfile():
     pass
 
     
-class Grades(models.Model):
+class Grade(models.Model):
     name = models.CharField("Class Name", max_length=50)
     number_of_students = models.IntegerField()
 
-class Students(models.Model):
+class Student(models.Model):
     student_name = models.CharField("Student Name",max_length=100)
     date_of_birth = models.DateField("Date Of Birth")
     address = models.CharField(max_length=100)
     phone = models.IntegerField("Phone Number")
-    student_grade = models.ForeignKey("Grades", on_delete=models.PROTECT, verbose_name="Student's Class")
+    student_grade = models.ForeignKey("Grade", on_delete=models.PROTECT, verbose_name="Student's Class")
     
 
-class Subjects(models.Model):
+class Subject(models.Model):
     name = models.CharField("Subject Name", max_length=50)
     code = models.CharField("Subject Code", max_length=10, unique=True)
-    grade = models.ForeignKey("Grades",on_delete=models.CASCADE, verbose_name="Subjects Grade")
+    grade = models.ForeignKey("Grade",on_delete=models.CASCADE, verbose_name="Subjects Grade")
     marks = models.IntegerField("Subjects Mark")
     
 
-class StudentsSubjects(models.Model):
-    student = models.ForeignKey(Students, on_delete=models.CASCADE, verbose_name="Student's Name")
-    subject = models.ForeignKey(Subjects, on_delete=models.CASCADE,verbose_name="Student's Subject")
+class StudentSubject(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name="Student's Name")
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE,verbose_name="Student's Subject")
     optional = models.BooleanField()
 
 
-class Exams(models.Model):
+class Exam(models.Model):
     exam_term_in_choice = [
         ('1st', 'First'),
         ('2nd','Second'),
@@ -40,14 +40,14 @@ class Exams(models.Model):
     date = models.DateField()
     name = models.CharField(max_length=50)
 
-class Results():
-    student = models.ForeignKey(Students, on_delete=models.CASCADE)
-    exam = models.ForeignKey(Exams, on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subjects, on_delete=models.CASCADE)
+class Result(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     mark = models.IntegerField()
     teachers_comment = models.CharField("Teacher's Comment", max_length=300)
 
-class ResultsComment():
-    exam = models.ForeignKey(Exams, on_delete=models.CASCADE)
-    student = models.ForeignKey(Students, on_delete=models.CASCADE)
+class ResultComment(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     result_comment = models.CharField(max_length=300)

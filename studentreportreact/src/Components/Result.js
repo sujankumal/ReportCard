3d77e4dae7a6370f121388ca32e3592e746032e4
Sujan Kumal <mailtosujankumal@gmail.com>
2020-03-16 19:24:02 +0545

    all student results done

--- studentreportreact/src/Components/Result.js
+++ studentreportreact/src/Components/Result.js
@@ -261,6 +261,9 @@ class StudentResult extends Component{
             gradevalue: e.target.value,
             studentresults:[],
         });
+        if(this.state.gradestudentresultclicked){
+            this.get_subjects_by_grade(e.target.value);
+        }
     }
 
     student_selected(e){
@@ -286,6 +289,11 @@ class StudentResult extends Component{
             this.get_student_result(studentid, e.target.value);
             let studentgrade = this.state.students.find(std=>std.id==studentid).student_grade;
             this.get_subjects_by_grade(studentgrade);
+        }
+        if(this.state.gradestudentresultclicked){
+            this.state.students.forEach((student, index)=>{ 
+                this.get_student_result_calledbyall(student.id, e.target.value, 'results'+index );
+            });
         }
         this.find_result_comment(e.target.value);
     }
@@ -401,68 +409,46 @@ class StudentResult extends Component{
         if(this.state.gradestudentresultclicked){
             // all students of grade
             let exam = this.state.examvalue;
-            if(exam && !this.state.allstudentsresultprocessisrunning){
-                this.setState({
-                    allstudentsresultprocessisrunning:true,
-                });
+            if(exam){
                 let exam_title = this.state.exams.find(item=> item.id == exam).name;
                 let students = this.state.students;
-                // let studentselected = this.state.studentvalue;
-                // let student = students.find(std => std.id == studentselected);
-                this.get_subjects_by_grade(this.state.gradevalue);
                 let payload = [];
                 let results = [];
                 students.forEach((student, index)=>{ 
-                    this.get_student_result_calledbyall(student.id, exam, 'results'+index );
-                    // this.get_subjects_by_grade_calledbyall()
-                    // results[index] = [];
-                    results[index] = this.state['student'+index];
-                    let gradesubjects = this.state.gradesubjects;
+                    const stateindex = 'results'+index;
+                    results[index] = this.state[stateindex];
                     let cgpa = [];
                     let data = <div className="container" key={index}>
                     <table className="table table-bordered table-striped table-hover table-sm">
                     <tbody>
                         <tr><td colSpan="10"><b>Exam Report: {exam_title}</b></td></tr>
-                        <tr>
-                            <td colSpan="5">Name: {student.student_name}</td>
+                        <tr><td colSpan="5">Name: {student.student_name}</td>
                             <td colSpan="2">Class: {this.state.grades.find(grade=>grade.id == student.student_grade).name}</td>
                             <td>Class Teacher Remarks</td>
                         </tr>
-                        <tr>
-                            <td>SN</td>
-                            <td>Subjects</td>
-                            <td>Theory</td>
-                            <td>Practical</td>
-                            <td>Total</td>
-                            <td>GPA</td>
-                            <td>Grade</td>
+                        <tr><td>SN</td><td>Subjects</td><td>Theory</td><td>Practical</td><td>Total</td><td>GPA</td><td>Grade</td>
                             <td rowSpan="0" >{
                                 (this.state.grades.find(grade=>grade.classteacher == this.props.userid))?
                                 <textarea rows="6" placeholder={this.resultcomment(student.id, exam)} className="form-control form-control-sm" onBlur={(e)=>this.update_result_comment(e, student.id, exam)}/>
                                 : this.resultcomment(student.id, exam)
                             }</td>
                         </tr>
-                        {
-                            (results[index] && results[index].length>0)?results[index].map((result,ind)=>{
+                        {console.log("stateindex",results, stateindex)}
+                        {(results[index])?results[index].map((result,ind)=>{
                                 let theory = Math.round(0.6*parseFloat(result.mark));
                                 let practical = Math.round( 0.4*parseFloat(result.cas));
                                 let total = theory +practical;
                                 let gpa = calculategrade(total);
                                 cgpa.push(gpa.gpa);
+                                console.log('inside',stateindex, cgpa);
                                 return <tr key={ind}>
                                     <td>{ind+1}</td>
                                     {/* Correct here and in home class mark display map */}
-                                    <td>{gradesubjects.map(subject=> (subject.id == result.subject)?subject.name:null)}</td>
-                                    <td>{theory}</td>
-                                    <td>{practical}</td>
-                                    <td>{total}</td>
-                                    <td>{gpa.gpa}</td>
-                                    <td>{gpa.grade}</td>
-
-                                </tr>
-                                }
-                                ):null          
-                            }
+                                    <td>{this.state.gradesubjects.forEach(subject=> (subject.id == result.subject)?subject.name:null)}</td>
+                                    <td>{theory}</td><td>{practical}</td><td>{total}</td><td>{gpa.gpa}</td><td>{gpa.grade}</td>
+                                    </tr>
+                            }):null
+                        }
                         </tbody>
                         <tfoot>
                         <tr><td colSpan="5"></td><th colSpan="2">GPA: {(cgpa.reduce((sum, gpa) => (gpa != 'FAIL')? sum + parseFloat(gpa): sum, 0)/cgpa.length).toFixed(2)}</th><td></td></tr>
@@ -475,7 +461,6 @@ class StudentResult extends Component{
                                 <b className="signature">Principal</b>
                             </td>
                         </tr>
-                        
                         </tfoot>
                     </table>
                     </div>
